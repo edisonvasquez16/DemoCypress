6111dad95ff7da5ee1a3f9e2a98c357df78f2da1
Edison Vasquez <vasq802548@siigo.com>
2023-03-24 10:20:00 -0500

    Add reportes mocha

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "7tauio",
-  //reporter: 'cypress-mochawesome-reporter',
+  reporter: 'cypress-mochawesome-reporter',
   chromeWebSecurity : false,
   e2e: {
     setupNodeEvents(on, config) {
-      //require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
