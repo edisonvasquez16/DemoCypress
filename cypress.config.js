471d78d9f57d949bb419e550b1c3ac6d19a8e0e3
Edison Vasquez <vasq802548@siigo.com>
2023-03-24 10:07:54 -0500

    Disabled videoReporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "7tauio",
-  reporter: 'cypress-mochawesome-reporter',
+  //reporter: 'cypress-mochawesome-reporter',
   chromeWebSecurity : false,
   e2e: {
     setupNodeEvents(on, config) {
