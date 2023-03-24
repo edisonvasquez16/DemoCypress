b743b7c7787bfb8a3cfa0d997cb268fe720d6792
Edison Vasquez <vasq802548@siigo.com>
2023-03-24 10:18:05 -0500

    Remove report of config

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "7tauio",
   //reporter: 'cypress-mochawesome-reporter',
-  chromeWebSecurity : false/*,
+  chromeWebSecurity : false,
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      //require('cypress-mochawesome-reporter/plugin')(on);
     },
-  },*/
+  },
 });
