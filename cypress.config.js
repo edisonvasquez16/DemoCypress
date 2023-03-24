829f3ade1943d37ed41e75e3882132bb1515e986
Edison Vasquez <vasq802548@siigo.com>
2023-03-24 11:05:08 -0500

    Solution report error

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,20 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "7tauio",
-  reporter: 'cypress-mochawesome-reporter',
-  chromeWebSecurity : false,
-  video: false,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: 'AutomationCypressReport',
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: true,
+    json: true,
+    quiet: true,
+  },
+  chromeWebSecurity: false,
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
+      //require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
