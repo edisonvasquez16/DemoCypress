da1b970025034f81c13894954d08da42f683b43d
Edison Vasquez <vasq802548@siigo.com>
2023-03-24 20:35:29 -0500

    Overwrite report

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   reporterOptions: {
     reportFilename: 'AutomationCypressReport',
     reportDir: 'cypress/reports',
-    overwrite: false,
+    overwrite: true,
     html: true,
     json: true,
   },
