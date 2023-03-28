66139d29109041e7c580a6f99760aa1469a9dae3
Edison Vasquez <vasq802548@siigo.com>
2023-03-28 18:10:23 -0500

    Other configuration

--- cypress.config.js
+++ cypress.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
       json: true,
   },
   chromeWebSecurity: false,
-  video: true,
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       //require('cypress-mochawesome-reporter/plugin')(on)
