3056879a94836ab4fd62d3eb796f9bc7e4ca5d08
Edison Vasquez <vasq802548@siigo.com>
2023-03-28 20:08:56 -0500

    Delete comments

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "7tauio",
   reporter: 'mochawesome',
-  //reporter: "cypress-multi-reporters",
   reporterOptions: {
       reportFilename: 'index',
       reportDir: 'cypress/results',
@@ -15,7 +14,7 @@ module.exports = defineConfig({
   video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      //require('cypress-mochawesome-reporter/plugin')(on)
+    
     },
   },
 });
